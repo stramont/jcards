08c330cffc510a8599063618ecdbeb572ce9b759
Sebastian Wayne Tramontana <stramont@terpmail.umd.edu>
2021-05-17 16:30:30 -0500

    login started

---
+++ jcards-frontend/src/components/Login.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './style.css';
+
+
+function Login(props) {
+
+    return (
+        <div class="login">
+            <div id="loginbox" class="centered">
+                Login component
+
+            </div>
+        </div>
+    );
+
+}
+
+export default Login;
