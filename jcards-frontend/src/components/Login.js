4fe129413b0f2fcc3434eee79d82c02859dc9931
Sebastian Wayne Tramontana <stramont@terpmail.umd.edu>
2021-05-18 13:30:16 -0500

    distinguishable login

--- jcards-frontend/src/components/Login.js
+++ jcards-frontend/src/components/Login.js
@@ -6,8 +6,23 @@ function Login(props) {
 
     return (
         <div class="login">
+            <div id="logo" class="centered">
+                <img src="jcards_logo.png" alt="j*cards" width="300" />
+            </div>
             <div id="loginbox" class="centered">
-                Login component
+                <div>
+                    <form id="loginform" >
+                        <label>
+                            Username <br></br>
+                            <input class="logininput" type="text" /> <br></br>
+                        </label>
+                        <label>
+                            Password <br></br>
+                            <input class="logininput" type="text" /> <br></br>
+                        </label>
+                        <input class="logininput" id="submit-btn-login" type="submit" value="Sign in" />
+                    </form>
+                </div>
 
             </div>
         </div>
